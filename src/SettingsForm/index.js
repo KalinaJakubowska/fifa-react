2e76fe77cad131f2046625b7aa799d83d3ca2cce
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-29 17:59:05 +0200

    replacing components

--- src/SettingsForm/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css"
-
-const SettingsForm = () => (
-<div></div>
-);
-
-export default SettingsForm;
--- src/SettingsPlayers/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css"
-
-const SettingsPlayers = () => (
-<div></div>
-);
-
-export default SettingsPlayers;
