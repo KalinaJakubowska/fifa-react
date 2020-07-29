a465314e9c6a3050d3e4533eb680736086ef240b
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-29 18:04:12 +0200

    adding Settings interaction

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./style.css"
+import SettingsForm from "./SettingsForm";
+import SettingsPlayers from "./SettingsPlayers";
+import SettingsStartButton from "./SettingsStartButton";
 
-const Settings = ({ form, players }) => (
-    <div>
-        {form}
-        {players}
+const Settings = ({ players, addNewPlayer, removePlayer }) => (
+    <div className="settings">
+        <div className="settings__container">
+            <h2 className="settings__header">tytuł</h2>
+        </div>
+        <SettingsForm
+          addNewPlayer={addNewPlayer}
+        />
+
+        <SettingsPlayers
+          players={players}
+          removePlayer={removePlayer}
+        />
+
+        <SettingsStartButton
+        players={players}
+        />
     </div>
 );
 
