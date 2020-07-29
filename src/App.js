284a59f0f85e8f103e022ad10f85a7de9d693253
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-29 18:04:56 +0200

    adding App.js interaction

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Settings from "./Settings";
-import SettingsForm from "./SettingsForm";
-import SettingsPlayers from "./SettingsPlayers";
 
 function App() {
+  const [players, setPlayers] = useState(
+    localStorage.getItem("players") === null ? "" : JSON.parse(localStorage.getItem("players"))
+  );
+
+  const removePlayer = (id) => {
+    setPlayers(players => players.filter(task => task.id !== id));
+  };
+
+  const addNewPlayer = (name) => {
+    setPlayers(players => [
+      ...players,
+      {
+        name,
+        done: false,
+        id: players.length ? players[players.length - 1].id + 1 : 1,
+      }
+    ])
+  };
+
   return (
     <Settings
-      form={
-        <SettingsForm />
-      }
-      players={
-        <SettingsPlayers />
-      } />
+      players={players}
+      addNewPlayer={addNewPlayer}
+      removePlayer={removePlayer}
+    />
+
 
   )
 }
