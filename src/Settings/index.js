13bd1c1cce13ce6d194832b342e7ef616960d17d
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-07-29 14:46:33 +0200

    adding Settings component

---
+++ src/Settings/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css"
+
+const Settings = ({ form, players }) => (
+    <div>
+        {form}
+        {players}
+    </div>
+);
+
+export default Settings;
