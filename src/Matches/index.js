fb733dbbd24828e22707dd44b2658deacf6a7f1b
Kalina Jakubowska <witillie@gmail.com>
2020-07-29 21:24:59 +0200

    list changed to divs

--- src/Matches/index.js
+++ src/Matches/index.js
@@ -5,13 +5,13 @@ const Matches = ({ matches, players }) => {
     return (
         <div className="matches">
             {matches && matches.map(({ id, player1, player2, goal1, goal2 }) =>
-                (<ul key={id} className="match">
-                    <li className="match__item">{player1}</li>
-                    <li className="match__item">{goal1}</li>
-                    <li className="match__item">VS</li>
-                    <li className="match__item">{goal2}</li>
-                    <li className="match__item">{player2}</li>
-                </ul>)
+                (<div key={id} className="match">
+                    <div className="match__item match__item--player">{player1}</div>
+                    <div className="match__item match__item--goal">{goal1}</div>
+                    <div className="match__item match__item--vs">VS</div>
+                    <div className="match__item match__item--goal">{goal2}</div>
+                    <div className="match__item match__item--player">{player2}</div>
+                </div>)
             )}
         </div>
     )
