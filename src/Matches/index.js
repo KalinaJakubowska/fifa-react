a1e9ab568fbdfd67b96ac39be328238088e71a58
Marcin Jakubowski <marcin.jakubowski137@gmail.com>
2020-08-04 12:46:11 +0200

    adding function to count players statistics

--- src/Matches/index.js
+++ src/Matches/index.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 
-const Matches = ({ matches, players }) => {
+const Matches = ({ matches, players, setPlayersStats }) => {
+
+    useEffect(() => {
+        addStats();
+    }, [matches]);
+
+    const addStats = () => {
+        let playersStatsTemplate = [];
+
+        for (const player of players) {
+            console.log(player);
+            playersStatsTemplate = [
+                ...playersStatsTemplate,
+                {
+                    name: player.name,
+                    goalsScored: 0,
+                    goalsConceded: 0,
+                    wins: 0,
+                    draws: 0,
+                    losses: 0,
+                    points: 0,
+                },
+            ]
+        }
+
+        for (const match of matches) {
+            console.log(match);
+            for (const playerStatsTemplate of playersStatsTemplate) {
+                if (match.player1 === playerStatsTemplate.name) {
+                    playerStatsTemplate.goalsScored += match.goal1;
+                    playerStatsTemplate.goalsConceded += match.goal2;
+                    if (match.goal1 > match.goal2) {
+                        playerStatsTemplate.wins += 1;
+                    }
+                    else if (match.goal1 < match.goal2) {
+                        playerStatsTemplate.losses += 1;
+                    }
+                    else if (match.goal1 === match.goal2) {
+                        playerStatsTemplate.draws += 1;
+                    }
+                }
+
+
+                if (match.player2 === playerStatsTemplate.name) {
+                    playerStatsTemplate.goalsScored += match.goal2;
+                    playerStatsTemplate.goalsConceded += match.goal1;
+                    if (match.goal2 > match.goal1) {
+                        playerStatsTemplate.wins += 1;
+                    }
+                    else if (match.goal2 < match.goal1) {
+                        playerStatsTemplate.losses += 1;
+                    }
+                    else if (match.goal1 === match.goal2) {
+                        playerStatsTemplate.draws += 1;
+                    }
+                }
+            }
+        }
+        setPlayersStats(playersStatsTemplate);
+    }
+
     return (
         <div className="matches">
             {matches && matches.map(({ id, player1, player2, goal1, goal2 }) =>
