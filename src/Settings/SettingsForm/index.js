4d13a61b31e382f711e4af9fa09e5f7fa8fb15a7
Kalina Jakubowska <witillie@gmail.com>
2020-08-19 19:35:47 +0200

    focusing input on form submit added

--- src/Settings/SettingsForm/index.js
+++ src/Settings/SettingsForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Button } from "./../../styledButtons.js";
 import { Input } from "./../../styledInput";
 import { Form } from "./styled.js";
@@ -6,12 +6,14 @@ import { Form } from "./styled.js";
 const SettingsForm = ({ addNewPlayer }) => {
 
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
         if (newTaskContent.trim().length > 0) {
             addNewPlayer(newTaskContent.trim());
             setNewTaskContent("");
+            inputRef.current.focus();
         }
     }
 
@@ -19,6 +21,7 @@ const SettingsForm = ({ addNewPlayer }) => {
         <Form className="settingsForm" onSubmit={onFormSubmit}>
             <Input
                 value={newTaskContent}
+                ref={inputRef}
                 autoFocus
                 placeholder="Podaj nazwę gracza"
                 onChange={({ target }) => setNewTaskContent(target.value)}
